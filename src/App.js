4ad67e4ae1b502be593a6ef9076d268e191959de
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-06-11 23:50:50 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { signal, computed } from "@preact/signals-react";
+import { Workspaces } from "./Workspaces";
+
+function createWorkspaceSignal(workspaces) {
+  const workspaceSignals = signal({});
+  Object.keys(workspaces).forEach((key) => {
+    const workspace = workspaces[key];
+    const workspaceSignal = signal(workspace);
+    const templateInfoSignal = signal(workspace.templateInfo);
+    const displayLabelSignal = signal(workspace.templateInfo.displayLabel);
+    const descriptionSignal = signal(workspace.templateInfo.description);
+
+    templateInfoSignal.value.displayLabel = displayLabelSignal;
+    templateInfoSignal.value.description = descriptionSignal;
+
+    workspaceSignal.value.templateInfo = templateInfoSignal;
+    workspaceSignals.value[key] = workspaceSignal;
+  });
+  return workspaceSignals;
+}
+
+const workspaceSignal = createWorkspaceSignal(Workspaces);
+
+const labels = computed(() => {
+  return Object.values(workspaceSignal.value).map((workspace) => {
+    return {
+      id: workspace.value.id,
+      labelName: workspace.value.templateInfo.value.displayLabel,
+    };
+  });
+});
+
+const descriptions = computed(() => {
+  return Object.values(workspaceSignal.value).reduce((acc , workspace) => {
+    return {
+      ...acc,
+      [workspace.value.id]: workspace.value.templateInfo.value.description
+    };
+  }, {});
+});
+
+console.log("descriptions", descriptions.value);
+
+const handleLabelChange = (event, workspaceId) => {
+  let currentLabel = labels.value.find((label) => label.id === workspaceId);
+  console.log("currentLabel =", currentLabel);
+  currentLabel.labelName.value = event.target.value;
+  console.log("labels.value=", labels.value);
+};
 
 function App() {
+  console.log("App rendered");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {labels.value.map((label, index) => {
+        return (
+          <div key={index}>
+            <input
+              type="text"
+              value={label.labelName}
+              onChange={(event) => handleLabelChange(event, label.id)}
+            />
+            <h1>Label: {label.labelName.value}</h1>
+            <h2>Description: {descriptions.value[label.id].value}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Workspaces.js
@@ -0,0 +1,16 @@
+export const Workspaces = {
+    'f342336a-3594-4cd1-9b0f-152888146783' : {
+        id: 'f342336a-3594-4cd1-9b0f-152888146783',
+        templateInfo: {
+            displayLabel: 'workspace 1',
+            description: 'This is workspace 1',
+        }
+    },
+    'f342336a-3594-4cd1-9b0f-152888146784' : {
+        id: 'f342336a-3594-4cd1-9b0f-152888146784',
+        templateInfo: {
+            displayLabel: 'workspace 2',
+            description: 'This is workspace 2',
+        }
+    },
+}
